d694c3ebb21e09112e6eaa19130855c8333163c1
eduardobc88 <eduardobc@Eduardos-Mac-mini.local>
2019-05-02 01:47:33 -0500

    sticky headers added and image color changed

--- dashboard-app/components/roles.vue
+++ dashboard-app/components/roles.vue
@@ -20,7 +20,10 @@
       </div>
     </div>
     <LoadingBar v-if="isLoading"/>
-    <BoxWrapper footerSize="32">
+    <BoxWrapper
+      footerSize="32"
+      style="padding-top: 0; padding-bottom: 0;"
+      >
       <PostListTable
         v-if="roles.models.length"
         v-bind:collection="roles"
--- dashboard-app/lib.js
+++ dashboard-app/lib.js
@@ -6,12 +6,12 @@ const getHexColor = (str) => {
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash)
   }
-  let colour = '#'
+  let colour = ''
   for (let i = 0; i < 3; i++) {
     let value = (hash >> (i * 8)) & 0xFF
     colour += ('00' + value.toString(16)).substr(-2)
   }
-  return 'background-color: ' + colour + ';'
+  return 'background-color: #' + colour + '80;'
 }
 
 const getAvatarURL = (fileURL) => {
