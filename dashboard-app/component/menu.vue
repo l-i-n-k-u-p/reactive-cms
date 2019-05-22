b4bbb3a3e2141341a2c653d78e08bdceb2dc2748
eduardobc88 <eduardobc@Eduardos-Mac-mini.local>
2019-05-21 23:30:43 -0500

    menu with sub-items

--- dashboard-app/component/menu.vue
+++ dashboard-app/component/menu.vue
@@ -13,18 +13,41 @@
         src="/website/assets/reactive-cms-logo.png"
         v-if="!isMenuSticky"
         />
-      <router-link
-        v-for="item in menuItems"
+      <div
+        v-for="(item, index) in menuItems"
         v-acl-show="item.resourceName"
-        v-bind:key="item.position"
-        :to="{ name: item.name, params: item.params }"
-        v-bind:class="getMenuItemClass(item)"
-      >
-        <i class="material-icons icon">
-          {{ item.icon }}
+        class="menu-option"
+        >
+        <router-link
+          v-bind:key="item.position"
+          :to="{ name: item.name, params: item.params }"
+          v-bind:class="getMenuItemClass(item)"
+        >
+          <i class="material-icons icon">
+            {{ item.icon }}
+          </i>
+          {{ item.title }}
+        </router-link>
+        <i
+          v-if="item.children"
+          class="material-icons button-more-items"
+          v-on:click="toggleOptions(index)"
+          >
+          {{ item.expanded ? 'expand_less' : 'expand_more' }}
         </i>
-        {{ item.title }}
-      </router-link>
+        <router-link
+          v-if="item.expanded"
+          class="children-item"
+          v-for="(itemChildren) of item.children"
+          v-bind:key="itemChildren.uuid"
+          :to="{ name: itemChildren.name, params: '' }"
+          >
+          <i class="material-icons icon">
+            {{ itemChildren.icon }}
+          </i>
+          {{ itemChildren.title }}
+        </router-link>
+      </div>
     </div>
     <div
       class="shadow"
@@ -50,6 +73,7 @@ export default {
           icon: 'dashboard',
           resourceName: 'dashboard',
           keys: 'dashboard',
+          expanded: false,
         },
         {
           position: 1,
@@ -59,6 +83,15 @@ export default {
           params: { page: 1 },
           resourceName: 'pages',
           keys: 'pages, page-detail, new-page',
+          expanded: false,
+          children: [
+            {
+              title: 'Add Page',
+              name: 'new-page',
+              icon: 'add',
+              uuid: this.$uuid.v1(),
+            },
+          ],
         },
         {
           position: 2,
@@ -68,6 +101,15 @@ export default {
           params: { page: 1 },
           resourceName: 'posts',
           keys: 'posts, post-detail, new-post',
+          expanded: false,
+          children: [
+            {
+              title: 'Add Post',
+              name: 'new-post',
+              icon: 'add',
+              uuid: this.$uuid.v1(),
+            },
+          ],
         },
         {
           position: 3,
@@ -77,6 +119,15 @@ export default {
           params: { page: 1 },
           resourceName: 'media',
           keys: 'media, media-detail, new-media',
+          expanded: false,
+          children: [
+            {
+              title: 'Add Media',
+              name: 'new-media',
+              icon: 'add',
+              uuid: this.$uuid.v1(),
+            },
+          ],
         },
         {
           position: 4,
@@ -86,6 +137,15 @@ export default {
           params: { page: 1 },
           resourceName: 'users',
           keys: 'users, user-detail, new-user',
+          expanded: false,
+          children: [
+            {
+              title: 'Add User',
+              name: 'new-user',
+              icon: 'add',
+              uuid: this.$uuid.v1(),
+            },
+          ],
         },
         {
           position: 5,
@@ -95,6 +155,15 @@ export default {
           params: { page: 1 },
           resourceName: 'roles',
           keys: 'roles, role-detail, new-role',
+          expanded: false,
+          children: [
+            {
+              title: 'Add Role',
+              name: 'new-role',
+              icon: 'add',
+              uuid: this.$uuid.v1(),
+            },
+          ],
         },
         {
           position: 6,
@@ -103,6 +172,7 @@ export default {
           icon: 'person',
           resourceName: '',
           keys: 'profile',
+          expanded: false,
         },
         {
           position: 7,
@@ -111,6 +181,7 @@ export default {
           icon: 'settings',
           resourceName: 'settings',
           keys: 'settings',
+          expanded: false,
         },
       ],
     }
@@ -128,22 +199,30 @@ export default {
     },
     highlightCurrentMenuItem: function(itemName) {
       let name = ''
-      for (let item of this.menuItems)
-        if (item.keys.indexOf(itemName) >= 0) {
-          name = item.name
-          break
+      let index = -1
+      for (let itemIndex in this.menuItems)
+        if (this.menuItems[itemIndex].keys.indexOf(itemName) >= 0) {
+          name = this.menuItems[itemIndex].name
+          index = itemIndex
         }
       this.currentItemName = name
+      this.menuItems[index].expanded = true
     },
     getMenuItemClass: function(item) {
       return {
-        'current': item.name == this.currentItemName,
+        'current': this.isCurrentItem(item),
         'option': true,
       }
+    },
+    isCurrentItem: function(item) {
+      return item.name == this.currentItemName
     },
     routerBeforeResolve: function(to, from, next) {
       this.highlightCurrentMenuItem(to.name)
       next()
+    },
+    toggleOptions: function(index) {
+      this.menuItems[index].expanded = !this.menuItems[index].expanded
     },
   },
 }
@@ -154,6 +233,7 @@ export default {
   flex-grow: 0;
   left: 0;
   min-width: 170px;
+  overflow-y: auto;
   position: fixed;
   z-index: 1;
 }
@@ -187,7 +267,7 @@ export default {
   display: flex;
   flex-direction: row;
   flex-shrink: 0;
-  font-size: 14px;
+  font-size: 13px;
   height: 32px;
   margin-left: 2px;
   margin-right: 2px;
@@ -196,7 +276,7 @@ export default {
   padding-right: 10px;
   position: relative;
   text-decoration: none;
-  text-transform: uppercase;
+  text-transform: capitalize;
   user-select: none;
   z-index: 2;
 }
@@ -226,5 +306,62 @@ export default {
   background-color: rgba(200, 200, 200, 0.20);
   color: #0010ff;
   font-weight: 500;
+}
+
+.children-item {
+  -webkit-user-select: none;
+  align-items: center;
+  border-bottom: 0;
+  border-radius: 3px;
+  border: 0;
+  color: #616161;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 0;
+  font-size: 12px;
+  font-weight: 400;
+  height: 32px;
+  margin-left: 2px;
+  margin-right: 2px;
+  max-width: 100%;
+  padding-left: 30px;
+  padding-right: 10px;
+  position: relative;
+  text-decoration: none;
+  text-transform: capitalize;
+  user-select: none;
+  z-index: 2;
+}
+
+.children-item:hover {
+  background-color: rgba(200, 200, 200, 0.20);
+  color: #0010ff;
+}
+
+.children-item .icon {
+  font-size: 18px;
+  margin: 0;
+  padding: 0;
+}
+
+.menu-option {
+  position: relative;
+}
+
+#menu .menu-option:last-child {
+  margin-bottom: 30px;
+}
+
+.button-more-items {
+  -webkit-user-select: none;
+  color: #616161;
+  cursor: pointer;
+  font-size: 18px;
+  position: absolute;
+  right: 8px;
+  top: 7px;
+  user-select: none;
+  z-index: 3;
 }
 </style>
